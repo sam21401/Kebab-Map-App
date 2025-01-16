688b5a82c443f97f8fc0641cfbb55e4ab20f533d
mikolajsniechowski <92689334+mikolajsniechowski@users.noreply.github.com>
2025-01-16 02:10:12 +0100

    25 creating user views (#26)
    * creating user sign up view
    
    * creating user register view
    
    * creating user panel view
    
    * user panel fix
    
    * switching between activities
    
    * lint cleanup

---
+++ app/src/main/java/com/example/kebabapp/fragments/user/UserLoginFragment.kt
@@ -0,0 +1,31 @@
+package com.example.kebabapp.fragments.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.kebabapp.R
+import com.example.kebabapp.databinding.FragmentUserLoginBinding
+
+class UserLoginFragment : Fragment() {
+    private lateinit var binding: FragmentUserLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        super.onCreate(savedInstanceState)
+        binding = FragmentUserLoginBinding.inflate(layoutInflater)
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_user_logging_to_navigation_user)
+        }
+        binding.tvRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_user_logging_to_navigation_user_register)
+        }
+
+        return binding.root
+    }
+}
